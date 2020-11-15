53f731751d8515e63f1f0392a0abea35640a9314
christianmaierger <c.maier1122@gmail.com>
2020-11-15 21:05:24 +0100

    edit added Datagrams, tried to work with ByteConverter, not really sure how to implement so far

--- AndroidTest/AndroidTest.java
+++ AndroidTest/AndroidTest.java
@@ -3,6 +3,8 @@ import java.util.Observer;
 
 import de.cmlab.ubicomp.lib.SensorUDPReceiver;
 import de.cmlab.ubicomp.lib.model.AndroidSensor;
+import de.cmlab.ubicomp.util.*;
+import java.net.*;
 
 /**
 * This class is for testing the SensorUDPReceiver
@@ -12,6 +14,29 @@ import de.cmlab.ubicomp.lib.model.AndroidSensor;
 public class AndroidTest {
 
 	public static void main(String[] args){
+		
+		// now I will try to fiddel around with a datagramSocket,
+		//as this seems to be necessary when sending/recieving data with UDP
+		
+		byte[] data = new byte[ 1024 ];
+		DatagramSocket socket;
+		DatagramPacket packet;
+		
+		try {
+			socket = new DatagramSocket( 5000 );
+		} catch (Exception e) {
+			return;
+		}
+		packet = new DatagramPacket( data, data.length );
+		
+		try {
+			socket.receive(packet);
+		} catch (Exception e) {
+			return;
+		}
+		
+		
+		
 		/*initiate a receiver by defining a port 
 		number that will be sent to the receiver from the app*/
 		// reciver is a Observable!
@@ -21,11 +46,21 @@ public class AndroidTest {
 		SensorUDPListener listener = new SensorUDPListener();
 		receiver.addObserver(listener);
 		
-		//Ich leg jetzt mal nen Sensor an und geb ihm wie im COnst gefordert eine Id
+		//here I instanciate a new Sensor with an id like the constructor states
+		// but not sure if I will need the instance
 		AndroidSensor firstSensor = new AndroidSensor("one");
+		// I try to get the Sensor data
 		float light = firstSensor.getAmbientlight();
-		// update wird auf einen Observer aufgerufen, was listener ist, aber 
-		listener.update(receiver, firstSensor);
+		
+		BytesToSensorValueConverter converter = new BytesToSensorValueConverter();
+		// converter instance is not needed it seems? As the following method is static!
+		float lightCon = BytesToSensorValueConverter.extractAmbientLightData(packet.getData());
+		
+		// update is called on a Observer, that is listener and takes a Observable,
+		// which is receiver here and an Object, that will be cast to AndroidSensor
+		// I am not sure what to take as Objet but probably converted bytes, but converter can
+		//not be cast to AndroidSensor?!? What should I pass as Object?
+		listener.update(receiver, packet);
 	}
 	
 	public static class SensorUDPListener implements Observer{
@@ -33,6 +68,7 @@ public class AndroidTest {
 		public SensorUDPListener() {
 			// TODO Auto-generated constructor stub
 			
+			//BytesToSensorValueConverter converter = new BytesToSensorValueConverter();
 			
 		}
 		
@@ -43,7 +79,7 @@ public class AndroidTest {
 			// real sensor data obtained e.g with getAmbientlight as float
 			AndroidSensor sensorValues = (AndroidSensor)arg;
 			System.out.println(sensorValues.getAmbientlight());
-			System.out.println(o.getAmbientlight());
+			System.out.println("test");
 			System.out.println(sensorValues.toString());
 			System.out.println("============================");
 		}
