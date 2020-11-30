7914beccccaf1568db2e1e04f7949a0828f38cd7
co <corina-angela.lehmann@stud.uni-bamberg.de>
2020-11-30 12:07:06 +0100

    edited build file

---
+++ assignment_02_06/src/de/cmlab/ubicomp/server/JavaServer.java
@@ -0,0 +1,35 @@
+package de.cmlab.ubicomp.server;
+
+import java.util.*;
+import org.apache.xmlrpc.WebServer;
+
+
+
+public class JavaServer {
+
+
+    public Integer sum(int x, int y){
+        return new Integer(x+y);
+    }
+
+    public Integer multiply(int x, int y){
+        return new Integer(x*y);
+    }
+
+    public static void main (String [] args){
+
+        try {
+
+            System.out.println("Attempting to start XML-RPC Server...");
+            WebServer server = new WebServer(8080);
+            server.addHandler("sample", new JavaServer());
+
+
+            server.start();
+            System.out.println("Started successfully.");
+            System.out.println("Accepting requests. (Halt program to stop.)");
+        } catch (Exception exception){
+            System.err.println("JavaServer: " + exception);
+        }
+    }
+}
