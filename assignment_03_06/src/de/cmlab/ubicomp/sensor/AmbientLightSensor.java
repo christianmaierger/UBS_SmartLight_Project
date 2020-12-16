3e370cdd99b463e7175dc41650be47e25a2fb51c
co <corina-angela.lehmann@stud.uni-bamberg.de>
2020-12-16 15:59:41 +0100

    tried with sensorUDP App

--- assignment_03_06/src/de/cmlab/ubicomp/sensor/AmbientLightSensor.java
+++ assignment_03_06/src/de/cmlab/ubicomp/sensor/AmbientLightSensor.java
@@ -1,9 +1,13 @@
 package de.cmlab.ubicomp.sensor;
 
-import java.io.IOException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Random;
+/*g6 erweiterung*/
+import java.io.*;
+import java.net.*;
+import java.nio.ByteBuffer;
+import java.nio.ByteOrder;
 
 import org.apache.xmlrpc.XmlRpcException;
 
@@ -21,6 +25,11 @@ public class AmbientLightSensor implements Runnable {
 
 	private boolean active = false;
 	private SensationConnector sensation;
+	
+	private float ambientlight;
+	private DatagramSocket serverSocket;
+    private byte[] receivedData; 
+    private DatagramPacket receivePacket;
 	
 	private String sensorID;
 	private String sensorClass;
@@ -62,7 +71,7 @@ public class AmbientLightSensor implements Runnable {
 		this.owner = "Group6";
 		this.comment = "Provides light intensity measurement in lux.";
 		this.activity = "active";
-		this.datatype = "String";
+		this.datatype = "Float";
 		this.minValue = "1.0";
 		this.maxValue = "10000.0";
 	}
@@ -118,25 +127,90 @@ public class AmbientLightSensor implements Runnable {
 	 */
 	@Override
 	public void run() {
-		while (active) {
-			SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
-			String dateStamp = df.format(new Date());
-			// dice a lux value
-			String light = this.randInt(1, 5000) + "." + this.randInt(1, 10);
-			// create sensor event
-			SensorEvent event = new SensorEvent(this.sensorID, dateStamp, light);
-			try {
-				// send to Sens-ation
-				sensation.notify(event);
-				// Sensor has a sampling interval of about 500ms
-				Thread.sleep(500);
-			} catch (Throwable e) {
-				active = false;
-				e.printStackTrace();
+		
+		try{
+            serverSocket = new DatagramSocket(8888);
+			receivedData = new byte[1024];
+            
+            while (active) {
+                receivePacket = new DatagramPacket(receivedData, receivedData.length);
+                try{
+                    serverSocket.receive(receivePacket);
+                    this.setAmbientlight(extractAmbientLightData(receivedData));
+                
+                    SimpleDateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.S");
+                    String dateStamp = df.format(new Date());
+                    // dice a lux value
+                    //String light = this.randInt(1, 5000) + "." + this.randInt(1, 10);
+                    String light = String.format("%f", this.getAmbientlight());
+                    // create sensor event
+                    SensorEvent event = new SensorEvent(this.sensorID, dateStamp, light);
+                    try {
+                        // send to Sens-ation
+                        sensation.notify(event);
+                        // Sensor has a sampling interval of about 500ms
+                        Thread.sleep(500);
+                    } catch (Throwable e) {
+                        active = false;
+                        e.printStackTrace();
+                    }
+                } catch (IOException e) {
+							
+                }    
+            }
+        } catch (SocketException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+    }
+    /** copied from BytesToSensorValueConverter.java, assignment 1
+     *
+     */
+	public static float extractAmbientLightData(byte[] receivedData){
+		float[] light = extractingData(56, 59, receivedData,4);
+        return light[0];
+	}
+	/** copied from BytesToSensorValueConverter.java, assignment 1
+     *
+     */
+	private static float[] extractingData(int startindex, int endindex, byte[] receivedData, int step )
+	{
+		int counter = 0;
+		int bigCounter = 0;
+		byte[] packet = new  byte[4];
+		double size = (((float)endindex-(float)startindex)+1)/4;
+		int resultsize = (int)Math.ceil(size);
+		float[] result = new float[resultsize];
+		
+		for(int i=startindex; i<endindex+1; i++){
+			if(counter < step-1){
+				packet[counter] = receivedData[i];
+				counter++;
+			}else{
+				float num = ByteBuffer.wrap(packet).order(ByteOrder.BIG_ENDIAN).getFloat();
+				result[bigCounter] = num;
+				counter = 0;
+				packet = new  byte[4];
+				bigCounter++;
 			}
 		}
+		return result;
+	}
+	
+    /**
+	 * @return the ambientlight
+	 */
+	public float getAmbientlight() {
+		return ambientlight;
 	}
 
+	/**
+	 * @param ambientlight the ambientlight to set
+	 */
+	public void setAmbientlight(float ambientlight) {
+		this.ambientlight = ambientlight;
+	}
+	
 	/**
 	 * Random integer generator
 	 * @param min lower bound
